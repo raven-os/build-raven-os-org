4007fcdc0b384b4c724a7acbbed3414158ce776c
mickael <mickael.bourgois@epitech.eu>
2019-07-21 23:10:43 +0200

    [Server Apidoc] Documented manifest/list

--- server/src/action/manifest/list.js
+++ server/src/action/manifest/list.js
@@ -3,6 +3,66 @@ const { query } = require('express-validator/check')
 const { sanitizeQuery } = require('express-validator/filter')
 const lowercase = require('../sanitizer/lowercase')
 
+/**
+ * @api {get} /api/manifest List
+ * @apiVersion 1.0.0
+ * @apiGroup Manifest
+ * @apiName ManifestList
+ *
+ * @apiDescription List all manifests
+ *
+ * @apiParam  (Query String) {String}                             [name]              Search results starting by provided name
+ * @apiParam  (Query String) {String='creation','update','name'}  [sort='creation']   Sort by provided column
+ * @apiParam  (Query String) {String='asc','desc'}                [dir='desc']        Direction of the sorting
+ * @apiParam  (Query String) {Integer}                            [per_page=15]       Number of items returned per page
+ * @apiParam  (Query String) {Integer}                            [page=1]            Page number
+ *
+ * @apiSuccess  {Object[]}  data                        List of manifests
+ * @apiSuccess  {Integer}   data.id                     Id of the manifest
+ * @apiSuccess  {String}    data.name                   Name of the manifest
+ * @apiSuccess  {String}    data.creation_date          Date of creation
+ * @apiSuccess  {String}    data.last_update            Date of last update
+ * @apiSuccess  {Object[]}  data.history                List of content (last updated first)
+ * @apiSuccess  {Integer}   data.history.id             Id of the content
+ * @apiSuccess  {Integer}   data.history.manifest_id    Id of the manifest
+ * @apiSuccess  {String}    data.history.content        The content of the manifest
+ * @apiSuccess  {String}    data.history.edition_date   Date of content edition
+ * @apiSuccess  {Object}    meta                        Metadata of the paginated result
+ * @apiSuccess  {Object}    meta.pagination             Pagination information
+ * @apiSuccess  {Integer}   meta.pagination.total       Total number of item found
+ * @apiSuccess  {Integer}   meta.pagination.perPage     Number of item per page
+ * @apiSuccess  {Integer}   meta.pagination.currentPage Current page
+ * @apiSuccess  {Integer}   meta.pagination.pageCount   Number of pages containing results
+ *
+ * @apiSuccessExample {json} Response
+ * {
+ *    "data": [
+ *      {
+ *         "id": 56,
+ *         "name": "to_compile",
+ *         "creation_date": "2019-07-19T22:25:10.370Z",
+ *         "last_update": "2019-07-19T22:25:10.370Z",
+ *         "history": [
+ *           {
+ *              "id": 75,
+ *              "manifest_id": 56,
+ *              "content": "{{content of a python file manifest}}"
+ *              "edition_date": "2019-07-19T22:25:10.370Z"
+ *           },
+ *           { ... }
+ *         ]
+ *      }
+ *    ],
+ *    "meta": {
+ *       "pagination": {
+ *          "total": 40,
+ *          "perPage": 15,
+ *          "currentPage": 1,
+ *          "pageCount": 3
+ *       }
+ *    }
+ * }
+ */
 class ListManifest extends AbstractAction {
   get validate () {
     return [
