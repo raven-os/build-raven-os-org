56ae5465c3b943397b34bea9eacff1cadac1ab56
Mickael Bourgois <35171168+BourgoisMickael@users.noreply.github.com>
2019-08-06 20:30:03 +0200

    [Server] Documented manifest endpoints with apidoc (#42)
    * [Server Apidoc] Added and setup apidoc
    
    * [Server Apidoc] Documented manifest/create
    
    * [Server Apidoc] Documented manifest/get
    
    * [Server Apidoc] Documented manifest/update
    
    * [Server Apidoc] Documented manifest/list
    
    * [Server Apidoc] Fixed size ranges of name and content
    
    * [Server] Improved apidoc descriptions for manifest endpoints
    
    Apply suggestions from code review
    
    Co-Authored-By: Arignir <Arignir@users.noreply.github.com>
    
    * [Front] Improved apidoc examples for manifest endpoints

--- server/src/action/manifest/create.js
+++ server/src/action/manifest/create.js
@@ -2,6 +2,67 @@ const AbstractAction = require('../abstract')
 const { body } = require('express-validator/check')
 const { sanitizeBody } = require('express-validator/filter')
 
+/**
+ * @api {post} /api/manifest Create
+ * @apiVersion 1.0.0
+ * @apiGroup Manifest
+ * @apiName ManifestCreate
+ *
+ * @apiDescription Create a new manifest that can be used to compile a package
+ *
+ * @apiParam  (Body) {String{1..255}}  name    Name of the manifest
+ * @apiParam  (Body) {String{1..}}     content Content of the manifest
+ *
+ * @apiParamExample {json} Request Body
+ * {
+ *    "name": "gcc",
+ *    "content": "{{content of a build manifest (python)}}"
+ * }
+ *
+ * @apiSuccess  {Integer}   id                    ID of the manifest
+ * @apiSuccess  {String}    name                  Name of the manifest
+ * @apiSuccess  {String}    creation_date         Date of creation
+ * @apiSuccess  {String}    last_update           Date of last update
+ * @apiSuccess  {Object[]}  history               List of content (ordered by last_update)
+ * @apiSuccess  {Integer}   history.id            ID of the content
+ * @apiSuccess  {Integer}   history.manifest_id   ID of the manifest
+ * @apiSuccess  {String}    history.content       The content of the manifest
+ * @apiSuccess  {String}    history.edition_date  Date of content edition
+ *
+ * @apiSuccessExample {json} Response
+ * {
+ *    "id": 56,
+ *    "name": "gcc",
+ *    "creation_date": "2019-07-19T22:25:10.370Z",
+ *    "last_update": "2019-07-19T22:25:10.370Z",
+ *    "history": [
+ *      {
+ *          "id": 75,
+ *          "manifest_id": 56,
+ *          "content": "{{content of a build manifest (python)}}"
+ *          "edition_date": "2019-07-19T22:25:10.370Z"
+ *      }
+ *    ]
+ * }
+ *
+ * @apiError  {String}    message        Error message
+ * @apiError  {Object[]}  errors         List of error details
+ * @apiError  {String}    errors.param   The request parameter that caused the error
+ * @apiError  {string}    errors.detail  Details about the parameter that caused the error
+ * @apiError  {string}    errors.value   Value provided for the invalid parameter
+ *
+ * @apiErrorExample {json} Bad Request 400
+ * {
+ *    "message": "Validation failed",
+ *    "errors": [
+ *      {
+ *          "param": "name",
+ *          "detail": "length must be between 1 and 255",
+ *          "value": ""
+ *      }
+ *    ]
+ * }
+ */
 class CreateManifest extends AbstractAction {
   get validate () {
     return [
