63a313fa8bdeb046419b25dde02edf230b720ea4
mickael <mickael.bourgois@epitech.eu>
2019-08-13 00:14:24 +0200

    [Builder] Refactored config

--- builder/src/config/config.js
+++ builder/src/config/config.js
@@ -1,12 +1,63 @@
-module.exports = {
-  nbuild_path: 'path_to_nbuild',
-  build_api_url: 'URL_OF_THE_API/',
-  manifest_dir: '/path/to/manifest/directory',
-  buffer_size: 2048,
-  output_dir: '/path/to/build/output/directory/',
-  nest_server_api_url: 'URL_OF_NEST_SERVER',
+const convict = require('convict')
+
+const config = convict({
+  nbuild_path: {
+    doc: 'Path to the nbuild executable',
+    format: String,
+    default: null,
+    env: 'NBUILD_PATH'
+  },
+  build_api_url: {
+    doc: 'URL of build-raven-os-org API',
+    format: String,
+    default: 'localhost:8000',
+    env: 'BUILD_API_URL'
+  },
+  manifest_dir: {
+    doc: 'Path to the directory where manifests are stored',
+    format: String,
+    default: './manifests/',
+    env: 'MANIFEST_DIR'
+  },
+  output_dir: {
+    doc: 'Path to the directory where the build\'s results are stored',
+    format: String,
+    default: './out/',
+    env: 'OUTPUT_DIR'
+  },
+  buffer_size: {
+    doc: 'Size of the bufferized build logs to send to build-raven-os-org',
+    format: Number,
+    default: 2048,
+    env: 'BUFFER_SIZE'
+  },
+  nest_server_api_url: {
+    doc: 'URL of nest-server to send built packages',
+    format: String,
+    default: 'localhost:8000',
+    env: 'NEST_SERVER_API_URL'
+  },
   apikeys: {
-    build: null,
-    nest_server: null
+    build: {
+      doc: 'Authentication apikey for communication with build-raven-os-org',
+      format: String,
+      default: 'DEFINE_BUILDER_APIKEY',
+      env: 'APIKEYS_BUILD'
+    },
+    nest_server: {
+      doc: 'Authentication apikey for communication with nest-server',
+      format: String,
+      default: 'DEFINE_NEST_SERVER_APIKEY',
+      env: 'APIKEYS_NEST_SERVER'
+    }
   }
-}
+})
+
+try {
+  const local = require('./config.local')
+  config.load(local)
+} catch (e) {}
+
+config.validate({ allowed: 'strict' })
+
+module.exports = config.getProperties()
--- builder/src/config/index.js
+++ builder/src/config/index.js
@@ -1,10 +1,1 @@
-const _ = require('lodash')
-
-const config = require('./config')
-let local = {}
-
-try {
-  local = require('./config.local')
-} catch (e) {}
-
-module.exports = _.merge({}, config, local)
+module.exports = require('./config')
