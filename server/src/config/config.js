1c291f7c7b3949d3d0691089b4653f76d4458633
mickael <mickael.bourgois@epitech.eu>
2019-08-12 23:47:13 +0200

    [Server] Refactored config file

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,23 +1,83 @@
-module.exports = {
-  port: 8000,
+const convict = require('convict')
+
+const config = convict({
+  port: {
+    doc: 'Port on which the server will listen',
+    format: 'port',
+    default: 8000,
+    env: 'PORT'
+  },
   database: {
     postgres: {
-      host: 'localhost',
-      port: 5432,
-      database: 'build-raven-os-org',
-      user: 'postgres',
-      password: 'postgres'
+      host: {
+        format: String,
+        default: 'localhost',
+        env: 'DATABASE_HOST'
+      },
+      port: {
+        format: 'port',
+        default: 5432,
+        env: 'DATABASE_PORT'
+      },
+      database: {
+        format: String,
+        default: 'build-raven-os-org',
+        env: 'POSTGRES_DB'
+      },
+      user: {
+        format: String,
+        default: 'postgres',
+        env: 'POSTGRES_USER'
+      },
+      password: {
+        format: String,
+        default: 'postgres',
+        env: 'POSTGRES_PASSWORD'
+      }
     },
     retry: {
-      count: 10,
-      interval: 3 * 1000 // 3 seconds
+      count: {
+        doc: 'Number of database connection retry if it fails',
+        format: Number,
+        default: 10,
+        env: 'DATABASE_RETRY_COUNT'
+      },
+      interval: {
+        doc: 'Interval of time in millisecond between each retry',
+        format: Number,
+        default: 3 * 1000,
+        env: 'DATABASE_RETRY_INTERVAL'
+      }
     }
   },
   cors: {
-    origin: ['http://localhost:8080']
+    origin: {
+      doc: 'Allowed origins for cors policy',
+      format: Array,
+      default: ['http://localhost:8080'],
+      env: 'CORS_ORIGIN'
+    }
   },
-  builder_apikey: 'DEFINE_BUILDER_APIKEY',
+  builder_apikey: {
+    doc: 'Private key to authenticate and authorize a builder',
+    format: String,
+    default: 'DEFINE_BUILDER_APIKEY',
+    env: 'BUILDER_APIKEY'
+  },
   pagination: {
-    default_item_per_page: 15
+    default_item_per_page: {
+      format: Number,
+      default: 15,
+      env: 'PAGINATION_DEFAULT_ITEM_PER_PAGE'
+    }
   }
-}
+})
+
+try {
+  const local = require('./config.local')
+  config.load(local)
+} catch (e) {}
+
+config.validate({ allowed: 'strict' })
+
+module.exports = config.getProperties()
