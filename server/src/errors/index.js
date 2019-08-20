457f772a8fba4fc464e536809aa87a7a3f9800e3
mickael <mickael.bourgois@epitech.eu>
2019-08-20 02:16:57 +0200

    server: add missing bad request error
    There was an error when the builder tried to use a bad request error
    that didn't exist

--- server/src/errors/index.js
+++ server/src/errors/index.js
@@ -1,10 +1,12 @@
 const NotFound = require('./not-found')
 const Forbidden = require('./forbidden')
+const BadRequest = require('./bad-request')
 
 class Errors {
   constructor (app) {
     this.NotFound = NotFound
     this.Forbidden = Forbidden
+    this.BadRequest = BadRequest
   }
 }
 
