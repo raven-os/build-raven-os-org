2e92b1cb3b0cc1c13d2cb18c00b6a64ece336545
mickael <mickael.bourgois@epitech.eu>
2019-09-01 21:26:50 +0200

    server: implement logout route

--- server/src/action/auth/index.js
+++ server/src/action/auth/index.js
@@ -1,8 +1,10 @@
 const Login = require('./login')
+const Logout = require('./logout')
 
 class AuthAction {
   constructor (app) {
     this.login = new Login(app)
+    this.logout = new Logout(app)
   }
 }
 
