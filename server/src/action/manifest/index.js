5f2e49451a526d7f39ee87ee7d07d3584bcdb454
mickael <mickael.bourgois@epitech.eu>
2019-11-02 17:49:14 +0100

    server: implement route to update manifest maintainer

--- server/src/action/manifest/index.js
+++ server/src/action/manifest/index.js
@@ -1,5 +1,6 @@
 const CreateManifest = require('./create')
 const UpdateManifest = require('./update')
+const UpdateManifestMaintainer = require('./update-maintainer')
 const GetManifest = require('./get')
 const ListManifest = require('./list')
 
@@ -7,6 +8,7 @@ class ManifestAction {
   constructor (app) {
     this.create = new CreateManifest(app)
     this.update = new UpdateManifest(app)
+    this.updateMaintainer = new UpdateManifestMaintainer(app)
     this.get = new GetManifest(app)
     this.list = new ListManifest(app)
   }
