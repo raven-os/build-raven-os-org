62a4c5269358de0e482e3c2b3c98f7a9be56023b
mickael <mickael.bourgois@epitech.eu>
2019-12-03 20:02:56 +0100

    apidoc: document auth endpoints

--- server/src/action/auth/login.js
+++ server/src/action/auth/login.js
@@ -1,6 +1,61 @@
 const AbstractAction = require('../abstract')
 const { body } = require('express-validator')
 
+/**
+ * @api {post} /api/auth/login  Login
+ * @apiVersion 1.0.0
+ * @apiGroup Auth
+ * @apiName AuthLogin
+ *
+ * @apiDescription Login
+ *
+ * @apiParam  (Body) {String}  email     User email
+ * @apiParam  (Body) {String}  password  Password
+ *
+ * @apiParamExample {json} Request Body
+ * {
+ *    "email": "john.doe@gmail.com",
+ *    "password": "$tR0nG_p4$$w0rd!"
+ * }
+ *
+ * @apiSuccess  {Integer}   id              ID of the user
+ * @apiSuccess  {String}    firstname       Firstname of the user
+ * @apiSuccess  {String}    lastname        Lastname of the user
+ * @apiSuccess  {String}    email           Email of the user
+ * @apiSuccess  {String[]}  rights          Rights of the user
+ * @apiSuccess  {String}    creation_date   Date of creation
+ * @apiSuccess  {String}    [last_access]   Last access date
+ *
+ * @apiSuccessExample {json} Response
+ * {
+ *    "id": 42,
+ *    "firstname": "John",
+ *    "lastname": "DOE",
+ *    "email": "john.doe@gmail.com",
+ *    "rights": ["admin"],
+ *    "creation_date": "2019-07-19T22:25:10.370Z",
+ *    "last_access": null
+ * }
+ *
+ * @apiError  {String}    message        Error message
+ * @apiError  {Object[]}  errors         List of error details
+ * @apiError  {String}    errors.param   The request parameter that caused the error
+ * @apiError  {string}    errors.detail  Details about the parameter that caused the error
+ * @apiError  {string}    errors.value   Value provided for the invalid parameter
+ *
+ * @apiErrorExample {json} Unauthorized 401
+ * {
+ *    "message": "You are already connected",
+ *    "errors": []
+ * }
+ *
+ * @apiErrorExample {json} Bad Request 400
+ * {
+ *    "message": "Wrong password",
+ *    "errors": []
+ * }
+ *
+ */
 class Login extends AbstractAction {
   get validate () {
     return [
