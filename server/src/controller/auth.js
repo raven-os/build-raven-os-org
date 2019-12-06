8490b955301338eea148f58ecc153c7edea77a64
mickael <mickael.bourgois@epitech.eu>
2019-12-06 01:21:35 +0100

    server: show reset password token timeout in email

--- server/src/controller/auth.js
+++ server/src/controller/auth.js
@@ -32,7 +32,7 @@ class AuthController {
     this.recovery[email] = { token, expireAt: date }
 
     const subject = 'Password recovery for Raven\'s Package Builder'
-    const text = `To reset your password on Raven's Package Builder, use this code: ${token}`
+    const text = `To reset your password on Raven's Package Builder, use this code: ${token} (available ${this.recoveryTimeout} minutes)`
 
     await this.app.mailer.send(email, subject, text)
   }
